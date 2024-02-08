08e2f9fd9953b08914a6b4aae53052955e2d1f8c
drkiettran <38035597+drkiet@users.noreply.github.com>
2024-02-08 06:44:47 -0500

    Updated common.h replacing __int64 with long

--- Common/common.h
+++ Common/common.h
@@ -29,25 +29,25 @@ inline std::chrono::steady_clock::time_point StopTimer()
     return std::chrono::steady_clock::now();
 }
 
-inline __int64 GetDurationInSeconds(std::chrono::steady_clock::time_point start, 
+inline long GetDurationInSeconds(std::chrono::steady_clock::time_point start, 
                                 std::chrono::steady_clock::time_point end)
 {
     return std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
 }
 
-inline __int64 GetDurationInMilliSeconds(std::chrono::steady_clock::time_point start, 
+inline long GetDurationInMilliSeconds(std::chrono::steady_clock::time_point start,
                                 std::chrono::steady_clock::time_point end)
 {
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 }
 
-inline __int64 GetDurationInMicroSeconds(std::chrono::steady_clock::time_point start, 
+inline long GetDurationInMicroSeconds(std::chrono::steady_clock::time_point start,
                                 std::chrono::steady_clock::time_point end)
 {
     return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 }
 
-inline __int64 GetDurationInNanoSeconds(std::chrono::steady_clock::time_point start, 
+inline long GetDurationInNanoSeconds(std::chrono::steady_clock::time_point start,
                                 std::chrono::steady_clock::time_point end)
 {
     return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
